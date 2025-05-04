cc87b5ea2c428f7ca6fa4f7338ccd4a7226680b4
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2025-05-04 17:09:53 +0000

    fc

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     ALCHEMICAL_DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 
-    WEBAUTHN_RP_ID = 'localhost'
+
     WEBAUTHN_RP_NAME = 'Flask WebAuthn Demo'
-    WEBAUTHN_RP_ORIGIN = 'http://localhost:5000'
+
+    # config.py (o donde toque)
+    WEBAUTHN_RP_ID       = "ubiquitous-garbanzo-w476wjv9qj929rpp-5000.app.github.dev"
+    WEBAUTHN_RP_ORIGIN   = "https://ubiquitous-garbanzo-w476wjv9qj929rpp-5000.app.github.dev"
